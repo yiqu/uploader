95ddb3380ea37e5bed4a663839921a70fe277976
Kevin Qu <kevinqu88@gmail.com>
2022-04-14 23:28:23 -0400

    firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,14 @@
 export const environment = {
   production: true,
-  appTitle: 'Uploader'
+  appTitle: 'Uploader',
+  firebaseConfig: {
+    apiKey: "AIzaSyBWgKOKv3hHf5VbkYNzf7VSJkApjOpsw2A",
+    authDomain: "udemy-angular-665a2.firebaseapp.com",
+    databaseURL: "https://udemy-angular-665a2.firebaseio.com",
+    projectId: "udemy-angular-665a2",
+    storageBucket: "udemy-angular-665a2.appspot.com",
+    messagingSenderId: "578777764146",
+    appId: "1:578777764146:web:d7cb0c981cc4273356072b",
+    measurementId: "G-VCTGBN86B1"
+  },
 };
